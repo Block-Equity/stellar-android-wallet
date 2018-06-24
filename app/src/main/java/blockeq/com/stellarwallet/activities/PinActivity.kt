af1954525f7887bd78e5b4bcd2fd448a25904488
mhamdaoui-twg <hamdaouismohamed@gmail.com>
2018-06-24 14:17:15 -0400

    Finalized PIN flow by adding a sample on how to use the PIN View

--- app/src/main/java/blockeq/com/stellarwallet/activities/PinActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/PinActivity.kt
@@ -3,15 +3,19 @@ package blockeq.com.stellarwallet.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import blockeq.com.stellarwallet.R
 import com.andrognito.pinlockview.PinLockListener
 import kotlinx.android.synthetic.main.activity_pin.*
-import android.view.animation.Animation
-
-
 
 class PinActivity : AppCompatActivity(), PinLockListener {
+
+    // Correct PIN example. later on, this would be fetched from the Local Storage
+    companion object {
+        const val CORRECT_PIN = "1234"
+        const val RESULT_FAIL = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +28,38 @@ class PinActivity : AppCompatActivity(), PinLockListener {
     }
 
     override fun onComplete(pin: String?) {
-        if (pin != "1234") {
-            indicatorDots.visibility = View.GONE
-            wrongPinDots.visibility = View.VISIBLE
+        if (pin != CORRECT_PIN) {
+            showWrongPinDots(true)
             val shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake)
             shakeAnimation.setAnimationListener(object : Animation.AnimationListener {
                 override fun onAnimationStart(arg0: Animation) {}
                 override fun onAnimationRepeat(arg0: Animation) {}
                 override fun onAnimationEnd(arg0: Animation) {
-                    wrongPinDots.visibility = View.GONE
-                    indicatorDots.visibility = View.VISIBLE
+                    showWrongPinDots(false)
                     pinLockView.resetPinLockView()
+                    setResult(RESULT_FAIL)
+                    finish()
+                    overridePendingTransition(R.anim.stay, R.anim.slide_out_down)
                 }
             })
             wrongPinDots.startAnimation(shakeAnimation)
         } else {
-
+            setResult(RESULT_OK)
+            finish()
+            overridePendingTransition(R.anim.stay, R.anim.slide_out_down)
         }
     }
 
     override fun onPinChange(pinLength: Int, intermediatePin: String?) {
+    }
+
+    private fun showWrongPinDots(show: Boolean) {
+        indicatorDots.visibility = if (show) View.GONE else View.VISIBLE
+        wrongPinDots.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun onBackPressed() {
+        setResult(RESULT_CANCELED)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/blockeq/com/stellarwallet/fragments/tabs/MyOffersTab.kt
+++ app/src/main/java/blockeq/com/stellarwallet/fragments/tabs/MyOffersTab.kt
@@ -11,7 +11,6 @@ import blockeq.com.stellarwallet.R
 class MyOffersTab : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab_my_offers, container, false)
     }
 }
