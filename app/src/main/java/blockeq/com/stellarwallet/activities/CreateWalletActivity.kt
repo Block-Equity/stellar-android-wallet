0c0b43773f19d9608491dc76d17004bf2ac026a6
Daniel Wang <danielw68@gmail.com>
2018-09-05 10:07:48 -0400

    Comitting temporary changes for sync up with dev

--- app/src/main/java/blockeq/com/stellarwallet/activities/CreateWalletActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/CreateWalletActivity.kt
@@ -2,6 +2,8 @@ package blockeq.com.stellarwallet.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import blockeq.com.stellarwallet.R
 
 class CreateWalletActivity : AppCompatActivity() {
@@ -9,5 +11,19 @@ class CreateWalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_wallet)
+
+        setSupportActionBar(findViewById(R.id.createToolbar))
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            if (item.itemId == android.R.id.home) {
+                finish()
+                return true
+            }
+        }
+        return false
     }
 }
--- app/src/main/java/blockeq/com/stellarwallet/activities/LoginActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/LoginActivity.kt
@@ -1,9 +1,13 @@
 package blockeq.com.stellarwallet.activities
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import blockeq.com.stellarwallet.R
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,7 +16,25 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         createWalletButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this@LoginActivity)
+            val walletLengthList = listOf("Use a 12 word recovery phrase", "Use a 24 word recovery phrase").toTypedArray()
+            builder.setTitle("Create Wallet")
+                    .setItems(walletLengthList, DialogInterface.OnClickListener { dialog, which ->
+                        // The 'which' argument contains the index position
+                        // of the selected item
 
+                        val walletLength = if (which == 0) {
+                            12
+                        } else {
+                            24
+                        }
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("walletLength", walletLength)
+                        startActivity(intent)
+                    })
+            val dialog = builder.create()
+            dialog.show()
         }
 
         recoverWalletButton.setOnClickListener {
