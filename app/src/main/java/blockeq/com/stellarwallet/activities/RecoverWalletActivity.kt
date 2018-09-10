b27736444e1429f1b7456f3ea7690cc47329f75e
Daniel Wang <danielw68@gmail.com>
2018-09-10 14:37:02 -0400

    Finish UI for recover wallet, with word count check

--- app/src/main/java/blockeq/com/stellarwallet/activities/RecoverWalletActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/RecoverWalletActivity.kt
@@ -1,42 +1,27 @@
 package blockeq.com.stellarwallet.activities
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.View
 import blockeq.com.stellarwallet.R
 import blockeq.com.stellarwallet.activities.PinActivity.Companion.PIN_REQUEST_CODE
 import kotlinx.android.synthetic.main.activity_recover_wallet.*
 
 
-class RecoverWalletActivity : AppCompatActivity() {
+class RecoverWalletActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recover_wallet)
 
-        setSupportActionBar(findViewById(R.id.recoverToolbar))
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
-        nextButton.setOnClickListener {
-            startActivityForResult(Intent(this, PinActivity::class.java), PIN_REQUEST_CODE)
-            overridePendingTransition(R.anim.slide_in_up, R.anim.stay)
-        }
+        setupUI()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PIN_REQUEST_CODE) {
-            when (resultCode) {
-                Activity.RESULT_OK -> {
-                    val intent = Intent(this, MainActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    startActivity(intent)
-                }
-                Activity.RESULT_CANCELED -> finish()
-                else -> finish()
-            }
+            finish()
         }
     }
 
@@ -48,5 +33,35 @@ class RecoverWalletActivity : AppCompatActivity() {
             }
         }
         return false
+    }
+
+    //region User Interface
+    override fun setupUI() {
+        setupToolbar()
+
+        nextButton.setOnClickListener {
+            val wordCount = getWordCount(phraseEditText.text.toString())
+            if (wordCount == 12 || wordCount == 24) {
+                startActivityForResult(Intent(this, PinActivity::class.java), PIN_REQUEST_CODE)
+                overridePendingTransition(R.anim.slide_in_up, R.anim.stay)
+            } else {
+                showErrorMessage()
+            }
+        }
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(findViewById(R.id.recoverToolbar))
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun showErrorMessage() {
+        invalidPhraseTextView.visibility = View.VISIBLE
+    }
+
+    //endregion
+
+    private fun getWordCount(word : String) : Int {
+        return word.split(" ".toRegex()).size
     }
 }
