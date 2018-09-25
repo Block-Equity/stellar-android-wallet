d7c432e638afb5514fef742c2754ca95ed9644ed
Daniel Wang <danielw68@gmail.com>
2018-09-25 15:26:12 -0400

    Set up base UI for assets screen

--- app/src/main/java/blockeq/com/stellarwallet/activities/WalletsActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/WalletsActivity.kt
@@ -1,11 +1,24 @@
 package blockeq.com.stellarwallet.activities
 
+import android.os.Bundle
 import blockeq.com.stellarwallet.R
+import kotlinx.android.synthetic.main.content_wallets.*
 
 class WalletsActivity : BasePopupActivity() {
 
     override fun setContent(): Int {
         return R.layout.content_wallets
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupUI()
+    }
+
+    override fun setupUI() {
+        titleText.text = getString(R.string.asset_title_text)
+    }
+    //endregion
 
 }
