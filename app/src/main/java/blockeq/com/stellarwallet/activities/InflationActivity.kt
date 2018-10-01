7d60fe2ef60fee2644c80eb3799366eafbf8cf9c
Daniel Wang <danielw68@gmail.com>
2018-10-01 11:02:01 -0400

    Display no network error handling

--- app/src/main/java/blockeq/com/stellarwallet/activities/InflationActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/InflationActivity.kt
@@ -7,6 +7,7 @@ import blockeq.com.stellarwallet.WalletApplication
 import blockeq.com.stellarwallet.helpers.Constants
 import blockeq.com.stellarwallet.interfaces.SuccessErrorCallback
 import blockeq.com.stellarwallet.services.networking.Horizon
+import blockeq.com.stellarwallet.utils.NetworkUtils
 import kotlinx.android.synthetic.main.activity_inflation.*
 
 class InflationActivity : BaseActivity() {
@@ -23,16 +24,20 @@ class InflationActivity : BaseActivity() {
         addressEditText.setText(Constants.INFLATION_DESTINATION)
 
         saveButton.setOnClickListener {
-            Horizon.Companion.JoinInflationDestination(object : SuccessErrorCallback {
-                override fun onSuccess() {
-                    Toast.makeText(this@InflationActivity, "Inflation destination set!", Toast.LENGTH_SHORT).show()
-                }
+            if (NetworkUtils(this).isNetworkAvailable()) {
+                Horizon.Companion.JoinInflationDestination(object : SuccessErrorCallback {
+                    override fun onSuccess() {
+                        Toast.makeText(this@InflationActivity, "Inflation destination set!", Toast.LENGTH_SHORT).show()
+                    }
 
-                override fun onError() {
-                    Toast.makeText(this@InflationActivity, "There was an error setting inflation destination.", Toast.LENGTH_SHORT).show()
-                }
+                    override fun onError() {
+                        Toast.makeText(this@InflationActivity, "There was an error setting inflation destination.", Toast.LENGTH_SHORT).show()
+                    }
 
-            }, addressEditText.text.toString()).execute()
+                }, addressEditText.text.toString()).execute()
+            } else {
+                NetworkUtils(this).displayNoNetwork()
+            }
         }
     }
 }
