41b7101a1cb1b35973ca55949db5605000b8c7b8
Daniel Wang <danielw68@gmail.com>
2018-10-02 14:42:41 -0400

    Fix login bug by checking for shared preferences existence

--- app/src/main/java/blockeq/com/stellarwallet/helpers/LocalStore.kt
+++ app/src/main/java/blockeq/com/stellarwallet/helpers/LocalStore.kt
@@ -10,6 +10,26 @@ import org.stellar.sdk.responses.AccountResponse
 
 class LocalStore(private val sharedPreferences: SharedPreferences, private val gson: Gson) {
 
+    var pinViewState: PinViewState?
+        get() = get<PinViewState>(KEY_PIN_DATA, PinViewState::class.java)
+        set(viewState) = set<PinViewState>(KEY_PIN_DATA, viewState!!)
+
+    var encryptedPhrase: String?
+        get() = get(KEY_ENCRYPTED_PHRASE)
+        set(encryptedPhrase) = set(KEY_ENCRYPTED_PHRASE, encryptedPhrase)
+
+    var publicKey: String?
+        get() = get(KEY_STELLAR_ACCOUNT_PUBLIC_KEY)
+        set(publicKey) = set(KEY_STELLAR_ACCOUNT_PUBLIC_KEY, publicKey)
+
+    var balances: Array<AccountResponse.Balance>?
+        get() = get(KEY_STELLAR_BALANCES_KEY, Array<AccountResponse.Balance>::class.java)
+        set(balances) = set(KEY_STELLAR_BALANCES_KEY, balances)
+
+    init {
+        balances = arrayOf()
+    }
+
     private companion object {
         const val KEY_ENCRYPTED_PHRASE = "kEncryptedPhrase"
         const val KEY_PIN_DATA = "kPinData"
@@ -54,21 +74,4 @@ class LocalStore(private val sharedPreferences: SharedPreferences, private val g
         editor.remove(KEY_PIN_DATA)
         editor.apply()
     }
-
-    var pinViewState: PinViewState?
-        get() = get<PinViewState>(KEY_PIN_DATA, PinViewState::class.java)
-        set(viewState) = set<PinViewState>(KEY_PIN_DATA, viewState!!)
-
-
-    var encryptedPhrase: String?
-        get() = get(KEY_ENCRYPTED_PHRASE)
-        set(encryptedPhrase) = set(KEY_ENCRYPTED_PHRASE, encryptedPhrase)
-
-    var publicKey: String?
-        get() = get(KEY_STELLAR_ACCOUNT_PUBLIC_KEY)
-        set(publicKey) = set(KEY_STELLAR_ACCOUNT_PUBLIC_KEY, publicKey)
-
-    var balances: Array<AccountResponse.Balance>?
-        get() = get(KEY_STELLAR_BALANCES_KEY, Array<AccountResponse.Balance>::class.java)
-        set(balances) = set(KEY_STELLAR_BALANCES_KEY, balances)
 }
