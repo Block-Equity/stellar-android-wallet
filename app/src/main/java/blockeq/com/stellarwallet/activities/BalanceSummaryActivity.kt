26f8df2d757da759f35dbd4f73b2ee32fcfe900f
Daniel Wang <danielw68@gmail.com>
2018-10-03 16:36:41 -0400

    Rename Balance activity

--- app/src/main/java/blockeq/com/stellarwallet/activities/BalanceSummaryActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/BalanceSummaryActivity.kt
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import blockeq.com.stellarwallet.R
 
-class BalanceActivity : AppCompatActivity() {
+class BalanceSummaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_balance)
+        setContentView(R.layout.activity_balance_summary)
     }
 }
