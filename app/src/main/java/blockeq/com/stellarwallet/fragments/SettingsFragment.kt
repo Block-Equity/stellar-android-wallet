2505ebeacaf11008368717635d2b10b24f523068
Daniel Wang <danielw68@gmail.com>
2018-10-04 10:50:37 -0400

    Add UI for View secret seed

--- app/src/main/java/blockeq/com/stellarwallet/fragments/SettingsFragment.kt
+++ app/src/main/java/blockeq/com/stellarwallet/fragments/SettingsFragment.kt
@@ -1,11 +1,14 @@
 package blockeq.com.stellarwallet.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import blockeq.com.stellarwallet.R
 import blockeq.com.stellarwallet.WalletApplication
+import blockeq.com.stellarwallet.activities.PinActivity
 import blockeq.com.stellarwallet.models.PinType
 import kotlinx.android.synthetic.main.fragment_settings.*
 
@@ -33,9 +36,24 @@ class SettingsFragment : BaseFragment() {
             launchPINView(PinType.VIEW_PHRASE, "", phrase, false)
         }
 
+        viewSeedButton.setOnClickListener {
+            val phrase = WalletApplication.localStore!!.encryptedPhrase!!
+            launchPINView(PinType.CHECK, "", phrase, false)
+        }
+
         clearWalletButton.setOnClickListener {
             val phrase = WalletApplication.localStore!!.encryptedPhrase!!
             launchPINView(PinType.CLEAR_WALLET, "", phrase, false)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PinActivity.PIN_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+
+            }
+        }
+    }
+
 }
