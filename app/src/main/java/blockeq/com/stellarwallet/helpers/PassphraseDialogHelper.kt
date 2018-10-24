483b97bfa2bf06b3c352dd56d7e97b07970966f9
Daniel Wang <danielw68@gmail.com>
2018-10-24 14:37:37 -0400

    Add DialogHelper for passphrase UI

---
+++ app/src/main/java/blockeq/com/stellarwallet/helpers/PassphraseDialogHelper.kt
@@ -0,0 +1,26 @@
+package blockeq.com.stellarwallet.helpers
+
+import android.app.Activity
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.widget.EditText
+import blockeq.com.stellarwallet.R
+
+class PassphraseDialogHelper(activity: Activity, listener: PassphraseDialogListener) : AlertDialog.Builder(activity) {
+
+    interface PassphraseDialogListener {
+        fun onOK(phrase: String)
+    }
+
+    init {
+        setTitle("Enter a passphrase")
+        setMessage("Passphrase along with mnemonic phrase will be used to derive your address.")
+
+        val viewInflated = LayoutInflater.from(activity).inflate(R.layout.passphrase_dialog, activity.findViewById(android.R.id.content), false)
+        val input = viewInflated.findViewById(R.id.passphraseEditText) as EditText
+        setView(viewInflated)
+
+        setPositiveButton("OK") { _, _ -> listener.onOK(input.text.toString()) }
+        setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+    }
+}
