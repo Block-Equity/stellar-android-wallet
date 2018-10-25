036b5156b25aa0a1e08a4848c3094301ae0d2fe5
Daniel Wang <danielw68@gmail.com>
2018-10-25 11:37:27 -0400

    Add confirmation for passphrase

--- app/src/main/java/blockeq/com/stellarwallet/helpers/PassphraseDialogHelper.kt
+++ app/src/main/java/blockeq/com/stellarwallet/helpers/PassphraseDialogHelper.kt
@@ -3,24 +3,57 @@ package blockeq.com.stellarwallet.helpers
 import android.app.Activity
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
 import blockeq.com.stellarwallet.R
 
-class PassphraseDialogHelper(activity: Activity, listener: PassphraseDialogListener) : AlertDialog.Builder(activity) {
+class PassphraseDialogHelper(activity: Activity, private val listener: PassphraseDialogListener) : AlertDialog.Builder(activity) {
+
+    private var isConfirm = false
+    private var inputEditText : EditText? = null
+    private var originalPhrase = ""
 
     interface PassphraseDialogListener {
         fun onOK(phrase: String)
     }
 
     init {
-        setTitle("Enter a passphrase")
-        setMessage("Passphrase along with mnemonic phrase will be used to derive your address.")
+        setTitle(context.getString(R.string.passphrase_dialog_title))
+        setMessage(context.getString(R.string.passphrase_dialog_info))
 
         val viewInflated = LayoutInflater.from(activity).inflate(R.layout.passphrase_dialog, activity.findViewById(android.R.id.content), false)
-        val input = viewInflated.findViewById(R.id.passphraseEditText) as EditText
+        inputEditText = viewInflated.findViewById(R.id.passphraseEditText) as EditText
         setView(viewInflated)
 
-        setPositiveButton(context.getString(R.string.ok)) { _, _ -> listener.onOK(input.text.toString()) }
-        setNegativeButton(context.getString(R.string.cancel)) { dialog, _ -> dialog.cancel() }
+        setPositiveButton(context.getString(R.string.ok)) { _, _ ->
+
+            listener.onOK(inputEditText!!.text.toString())
+        }
+        setNegativeButton(context.getString(R.string.cancel)) { dialog, _ ->
+            dialog.cancel()
+        }
+    }
+
+    override fun show(): AlertDialog {
+        val dialog = super.show()
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val phrase = inputEditText!!.text.toString()
+            if (isConfirm) {
+                if (originalPhrase == phrase) {
+                    listener.onOK(phrase)
+                    dialog.cancel()
+                } else {
+                    dialog.setMessage(context.getString(R.string.passphrase_dialog_error))
+                    inputEditText!!.setText("")
+                }
+            } else {
+                originalPhrase = phrase
+                inputEditText!!.setText("")
+                dialog.setMessage(context.getString(R.string.passphrase_dialog_confirm))
+                isConfirm = true
+            }
+        }
+        return dialog
+
     }
 }
