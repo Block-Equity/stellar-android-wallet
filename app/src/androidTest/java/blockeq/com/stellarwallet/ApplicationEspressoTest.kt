b7e6b633dfda7c309b1dca576279ccbf9e4b6024
Davidbcn <david@nordicloop.com>
2018-10-31 19:05:37 -0400

    fixing some nonsense issues with espresso tests.

--- app/src/androidTest/java/blockeq/com/stellarwallet/ApplicationEspressoTest.kt
+++ app/src/androidTest/java/blockeq/com/stellarwallet/ApplicationEspressoTest.kt
@@ -1,12 +1,12 @@
 package blockeq.com.stellarwallet
 
 import android.content.Context
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.matcher.RootMatchers.isDialog
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.RootMatchers
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
@@ -15,10 +15,14 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.IllegalStateException
 
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
 @RunWith(AndroidJUnit4::class)
-class ApplicationTest {
+class ApplicationEspressoTest {
     private lateinit var context : Context
 
     @Rule
@@ -39,32 +43,32 @@ class ApplicationTest {
 
     @Test
     fun testCreateWalletOption12Words() {
-        onView(withId(R.id.createWalletButton)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.createWalletButton)).perform(ViewActions.click())
 
         createWalletFlow(MnemonicType.WORD_12, "1234")
 
-        onView(withId(R.id.nav_settings)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.nav_settings)).perform(ViewActions.click())
 
-        onView(withId(R.id.clearWalletButton)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.clearWalletButton)).perform(ViewActions.click())
 
         writePin("1234")
 
-        onView(withId(R.id.createWalletButton))
+        Espresso.onView(ViewMatchers.withId(R.id.createWalletButton))
     }
 
     @Test
     fun testCreateWalletOption24Words() {
-        onView(withId(R.id.createWalletButton)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.createWalletButton)).perform(ViewActions.click())
 
         createWalletFlow(MnemonicType.WORD_24, "1234")
 
-        onView(withId(R.id.nav_settings)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.nav_settings)).perform(ViewActions.click())
 
-        onView(withId(R.id.clearWalletButton)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.clearWalletButton)).perform(ViewActions.click())
 
         writePin("1234")
 
-        onView(withId(R.id.createWalletButton))
+        Espresso.onView(ViewMatchers.withId(R.id.createWalletButton))
     }
 
     enum class MnemonicType {
@@ -78,24 +82,24 @@ class ApplicationTest {
             MnemonicType.WORD_24 -> context.getString(R.string.create_word_option_2)
         }
 
-        onView(withText(optionString))
-                .inRoot(isDialog())
-                .check(matches(isDisplayed()))
-                .perform(click())
+        Espresso.onView(ViewMatchers.withText(optionString))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+                .perform(ViewActions.click())
 
-        onView(withId(R.id.confirmButton)).perform(click())
-
-        writePin("1234")
+        Espresso.onView(ViewMatchers.withId(R.id.confirmButton)).perform(ViewActions.click())
 
         writePin("1234")
 
-        onView(withId(R.id.navigationView))
+        writePin("1234")
+
+        Espresso.onView(ViewMatchers.withId(R.id.navigationView))
 
     }
 
     private fun writePin(PIN : String) {
         if (PIN.length != 4) {
-           throw IllegalStateException("PIN has to have 4 characters, now it has " + PIN.length)
+            throw IllegalStateException("PIN has to have 4 characters, now it has " + PIN.length)
         }
 
         writeIndividualPinNumber(PIN[0])
@@ -105,9 +109,9 @@ class ApplicationTest {
     }
 
     private fun writeIndividualPinNumber(number : Char) {
-        onView(withId(R.id.pinLockView))
+        Espresso.onView(ViewMatchers.withId(R.id.pinLockView))
                 .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder> (
-                        hasDescendant(withText(number.toString())), click()))
+                        ViewMatchers.hasDescendant(ViewMatchers.withText(number.toString())), ViewActions.click()))
     }
 
 }
