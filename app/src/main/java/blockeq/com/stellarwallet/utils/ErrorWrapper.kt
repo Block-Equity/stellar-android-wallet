103fe652e3ece9844c8fbf7cfc842386e0bba130
Daniel Wang <danielw68@gmail.com>
2018-11-08 18:06:29 -0500

    Extract readable strings

--- app/src/main/java/blockeq/com/stellarwallet/utils/ErrorWrapper.kt
+++ app/src/main/java/blockeq/com/stellarwallet/utils/ErrorWrapper.kt
@@ -2,13 +2,12 @@ package blockeq.com.stellarwallet.utils
 
 import android.content.Context
 import blockeq.com.stellarwallet.R
-import blockeq.com.stellarwallet.helpers.Constants
 
 class ErrorWrapper(context: Context) {
     var map: MutableMap<String, String> = mutableMapOf()
 
     init {
-        map[context.getString(R.string.tx_failed)] = "Transaction failed"
-        map[context.getString(R.string.op_underfunded)] = "Payment is underfunded"
+        map[context.getString(R.string.technical_tx_failed)] = context.getString(R.string.readable_tx_failed)
+        map[context.getString(R.string.technical_op_underfunded)] = context.getString(R.string.readable_op_underfunded)
     }
 }
