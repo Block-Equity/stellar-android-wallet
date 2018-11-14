ac6d4988abeb6a5f596a8f5baab561487b67266d
dabitdev <david@blockeq.com>
2018-11-14 17:42:24 -0500

    adding a dialog in LaunchActivity and WalletActivity to inform when a new app (new package) will be able.

--- app/src/main/java/blockeq/com/stellarwallet/activities/LaunchActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/LaunchActivity.kt
@@ -1,17 +1,61 @@
 package blockeq.com.stellarwallet.activities
 
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.Toast
 import blockeq.com.stellarwallet.R
+import blockeq.com.stellarwallet.helpers.Constants
+import blockeq.com.stellarwallet.models.SupportedAsset
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LaunchActivity : BaseActivity() {
+    private lateinit var newAppDialog : AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         setupUI()
+
+        checkNewApp()
+    }
+
+
+    private fun checkNewApp() {
+        val builder = AlertDialog.Builder(this@LaunchActivity)
+        builder.setTitle("The Beta program has ended")
+        builder.setMessage("The current app is not longer maintained, please install the new app")
+        builder.setPositiveButton("Download") { _, _ ->
+            val appPackageName = packageName // getPackageName() from Context or Activity object
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")));
+            } catch (e : ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")));
+            }
+        }
+        builder.setNegativeButton("Later", null)
+        val queue = Volley.newRequestQueue(this)
+
+        val request = StringRequest(Request.Method.GET, "https://play.google.com/store/apps/details?id=blockeq.com.stellarwallet", Response.Listener<String> {
+            if (!isFinishing) {
+                newAppDialog.show()
+            }
+        }, null)
+
+        newAppDialog = builder.create()
+        queue.add(request)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -71,6 +115,13 @@ class LaunchActivity : BaseActivity() {
                 }
         val dialog = builder.create()
         dialog.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (newAppDialog.isShowing) {
+            newAppDialog.dismiss()
+        }
     }
 
     //endregion
--- app/src/main/java/blockeq/com/stellarwallet/activities/WalletActivity.kt
+++ app/src/main/java/blockeq/com/stellarwallet/activities/WalletActivity.kt
@@ -1,20 +1,57 @@
 package blockeq.com.stellarwallet.activities
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import blockeq.com.stellarwallet.R
 import blockeq.com.stellarwallet.fragments.SettingsFragment
 import blockeq.com.stellarwallet.fragments.TradingFragment
 import blockeq.com.stellarwallet.fragments.WalletFragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class WalletActivity : BaseActivity() {
+    private lateinit var newAppDialog : AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallet)
 
         setupUI()
+
+        checkNewApp()
+    }
+
+
+    private fun checkNewApp() {
+        val builder = AlertDialog.Builder(this@WalletActivity)
+        builder.setTitle("The Beta program has ended")
+        builder.setMessage("The current app is not longer maintained. Please make sure you have written down your recovery phrase (12-24 words) or saved a copy of your secret key. After you have saved a back-up, please delete this app and then recover your wallet using the new version here")
+        builder.setPositiveButton("Download") { _, _ ->
+            val appPackageName = packageName // getPackageName() from Context or Activity object
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")));
+            } catch (e : ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")));
+            }
+        }
+        builder.setNegativeButton("Later", null)
+        val queue = Volley.newRequestQueue(this)
+
+        val request = StringRequest(Request.Method.GET, "https://play.google.com/store/apps/details?id=blockeq.com.stellarwallet", Response.Listener<String> {
+            if (!isFinishing) {
+                newAppDialog.show()
+            }
+        }, null)
+
+        newAppDialog = builder.create()
+        queue.add(request)
     }
 
     //region Navigation
@@ -44,8 +81,6 @@ class WalletActivity : BaseActivity() {
         transaction.commit()
     }
 
-    //endregion
-
     private fun setupUI() {
         setupNav()
     }
@@ -56,4 +91,11 @@ class WalletActivity : BaseActivity() {
         bottomNavigation.selectedItemId = R.id.nav_wallet
     }
     //endregion
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (newAppDialog.isShowing) {
+            newAppDialog.dismiss()
+        }
+    }
 }
