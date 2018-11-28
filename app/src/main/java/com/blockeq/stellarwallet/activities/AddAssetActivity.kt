a12a2b326297f0cea2e5dfafc4046dd544f5433f
Daniel Wang <danielw68@gmail.com>
2018-11-28 11:00:22 -0500

    Create AddAssetActivity

---
+++ app/src/main/java/com/blockeq/stellarwallet/activities/AddAssetActivity.kt
@@ -0,0 +1,12 @@
+package com.blockeq.stellarwallet.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddAssetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_asset)
+    }
+}
