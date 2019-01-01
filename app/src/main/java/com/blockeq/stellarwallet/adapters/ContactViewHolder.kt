de7bb6854e6485eeba702f2fec626ee26b2eb848
dabitdev <imdavidfernandez@gmail.com>
2019-01-01 23:40:09 +0100

    adding circle to each contact row.

--- app/src/main/java/com/blockeq/stellarwallet/adapters/ContactViewHolder.kt
+++ app/src/main/java/com/blockeq/stellarwallet/adapters/ContactViewHolder.kt
@@ -1,18 +1,22 @@
 package com.blockeq.stellarwallet.adapters
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
-
 import com.blockeq.stellarwallet.R
-import com.blockeq.stellarwallet.activities.StellarAddressActivity
 import com.blockeq.stellarwallet.activities.SendActivity
+import com.blockeq.stellarwallet.activities.StellarAddressActivity
 import com.blockeq.stellarwallet.models.Contact
+import com.github.abdularis.civ.CircleImageView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import de.hdodenhof.circleimageview.CircleImageView
-import java.util.*
+import timber.log.Timber
+
 
 /**
  * Contains a Contact List Item
@@ -20,22 +24,39 @@ import java.util.*
 class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val image: CircleImageView = itemView.findViewById<View>(R.id.rounded_iv_profile) as CircleImageView
     private val label: TextView = itemView.findViewById<View>(R.id.tv_label) as TextView
+    private val letter: TextView = itemView.findViewById<View>(R.id.contact_letter) as TextView
     private val button: TextView = itemView.findViewById<View>(R.id.button_add_address) as TextView
 
     private var mBoundContact: Contact? = null // Can be null
-    val colors: IntArray = intArrayOf(R.color.terracotta, R.color.lightBlue, R.color.puce, R.color.mantis, R.color.purple, R.color.pink)
+    private val colors: IntArray = intArrayOf(R.color.terracotta, R.color.lightBlue, R.color.puce, R.color.mantis, R.color.brown,
+            R.color.purple, R.color.pink, R.color.lightBlue, R.color.paleSky, R.color.apricot, R.color.cornflowerBlue)
     fun bind(contact: Contact) {
         mBoundContact = contact
         val appContext = label.context.applicationContext
         label.text = contact.name
-        if (contact.profilePic != null) {
-            Picasso.get().load(contact.profilePic).into(image)
-        } else {
-            image.circleBackgroundColor = ContextCompat.getColor(appContext, colors[getRandomNumber(0, colors.size)])
-        }
+        image.visibility = View.GONE
+        Picasso.get().load(contact.profilePic).into(image, object : Callback {
+            override fun onSuccess() {
+                letter.text = null
+                image.visibility = View.VISIBLE
+            }
+
+            override fun onError(e: Exception?) {
+                contact.name?.let {
+                    if (it.length > 1) {
+                        letter.text = it[0].toString()
+                        image.setImageBitmap(createImage(50, 50, getColor(appContext, it[0])))
+                        image.visibility = View.VISIBLE
+                    } else {
+                        letter.text = null
+                    }
+                }
+            }
+        })
+
         val stellarAddress = contact.stellarAddress
         if (stellarAddress == null) {
-            button.text = appContext.getString(R.string.add_address)
+            button.text = appContext.getString(R.string.add_stellar_address)
             button.setBackgroundColor(ContextCompat.getColor(appContext, R.color.blue1))
         } else {
             button.text = appContext.getString(R.string.send_payment)
@@ -59,10 +80,17 @@ class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         }
     }
 
-
-    private fun getRandomNumber(min: Int, max: Int): Int {
-        return Random().nextInt(max - min + 1) + min
+    fun getColor(context : Context, char : Char) : Int {
+        val index = char.toInt() % colors.size
+        return ContextCompat.getColor(context, colors[index])
     }
 
-
+    fun createImage(width: Int, height: Int, color: Int): Bitmap {
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+        paint.color = color
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+        return bitmap
+    }
 }
