70dbf76370b59c77098dc2d1b46b7d1e345652f3
dabitdev <imdavidfernandez@gmail.com>
2019-01-21 11:31:24 -0500

    adding dialog to invite the user to go to settings when the permissions are muted.

--- app/src/main/java/com/blockeq/stellarwallet/fragments/ContactsFragment.kt
+++ app/src/main/java/com/blockeq/stellarwallet/fragments/ContactsFragment.kt
@@ -1,8 +1,10 @@
 package com.blockeq.stellarwallet.fragments
 
 import android.Manifest
+import android.app.AlertDialog
 import android.arch.lifecycle.Observer
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -12,7 +14,6 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
-import com.blockeq.stellarwallet.R
 import com.blockeq.stellarwallet.activities.StellarAddressActivity
 import com.blockeq.stellarwallet.adapters.ContactsAdapter
 import com.blockeq.stellarwallet.helpers.OnTextChanged
@@ -22,6 +23,10 @@ import com.blockeq.stellarwallet.views.RecyclerSectionItemDecoration
 import com.blockeq.stellarwallet.vmodels.ContactsRepositoryImpl
 import kotlinx.android.synthetic.main.fragment_contact_list.*
 import timber.log.Timber
+import android.net.Uri
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import com.blockeq.stellarwallet.R
+
 
 /**
  * Fragment that holds the RecyclerView
@@ -35,7 +40,10 @@ class ContactsFragment : Fragment() {
     // Defines a variable for the search string
     private lateinit var appContext : Context
     private var currentContactList = ArrayList<Contact>()
+
+    private lateinit var searchButton : MenuItem
     private lateinit var refreshButton : MenuItem
+    private lateinit var addContactButton : MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,8 +65,9 @@ class ContactsFragment : Fragment() {
             rv_contact_list.layoutManager =  LinearLayoutManager(it)
         }
 
-        setInitialState()
+        checkRationale()
         requestContacts()
+
         //This logic around clearbutton is hack to fix #191, it should be removed if the bug is approved fix and relesaead.
         // https://github.com/mancj/MaterialSearchBar/issues/104
         val clearButton = searchBar.findViewById<View>(R.id.mt_clear)
@@ -78,26 +87,30 @@ class ContactsFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         // Inflate the menu to use in the action bar
         inflater.inflate(R.menu.contacts_fragment_menu, menu)
-        refreshButton = menu.findItem(R.id.refresh)
+        refreshButton = menu.findItem(R.id.refresh_contacts)
+        searchButton = menu.findItem(R.id.search_contacts)
+        addContactButton = menu.findItem(R.id.add_contact)
+
+        setMenuItemsEnable(false)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
-            R.id.refresh -> {
-                setInitialState()
+            R.id.refresh_contacts -> {
+                setInitialStateContacts()
                 refreshButton.isEnabled = false
                 showContacts(true)
                 return true
             }
-            R.id.add -> {
+            R.id.add_contact -> {
                 activity?.let {
                     startActivity(StellarAddressActivity.createContact(it))
                 }
                 return true
             }
-            R.id.search -> {
+            R.id.search_contacts -> {
                 viewFlipper.showNext()
                 searchBar.enableSearch()
                 searchBar.setOnSearchActionListener(object: OnSearchStateListener() {
@@ -114,13 +127,42 @@ class ContactsFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun setEnablePermissionsState(shouldShowRationale: Boolean) {
+        rv_contact_list.visibility = View.GONE
+        empty_view.visibility = View.GONE
+        progress_view.visibility = View.GONE
+        enable_permissions.visibility = View.VISIBLE
+        allow_permissions_button.setOnClickListener { it ->
+            if (shouldShowRationale) {
+                requestContacts()
+            } else {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Contact Permission Needed")
+                builder.setMessage("Open Settings, then tap Permissions and turn on Contacts.")
+                builder.setPositiveButton("Open Settings") { _, _ ->
+                    context?.let {that ->
+                        val intent = getAppSettingsIntent(appContext.packageName)
+                        startActivity(intent)
+                    }
+                }.create().show()
 
-    private fun setInitialState() {
+            }
+        }
+    }
+
+    private fun setMenuItemsEnable(isEnabled : Boolean) {
+        refreshButton.isEnabled = isEnabled
+        searchButton.isEnabled = isEnabled
+        addContactButton.isEnabled = isEnabled
+    }
+
+    private fun setInitialStateContacts() {
+        setMenuItemsEnable(true)
         rv_contact_list.visibility = View.GONE
         empty_view.visibility = View.GONE
         progress_view.visibility = View.VISIBLE
+        enable_permissions.visibility = View.GONE
     }
-
 
     private fun requestContacts() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && hasPermissions(appContext, Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
@@ -142,15 +184,22 @@ class ContactsFragment : Fragment() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == PERMISSIONS_REQUEST_CONTACTS) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 showContacts()
             } else {
+                checkRationale()
                 Timber.e("Permissions Access denied")
             }
         }
     }
 
+    private fun checkRationale() {
+        val shouldShowRationale = shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) || shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)
+        setEnablePermissionsState(shouldShowRationale)
+    }
+
     private fun showContacts(forceRefresh: Boolean = false) {
+        setInitialStateContacts()
         ContactsRepositoryImpl(appContext).getContactsListLiveData(forceRefresh).observe(viewLifecycleOwner, Observer {
             Timber.d("Observer triggered {${it?.stellarContacts?.size}")
             it?.let { that ->
@@ -167,9 +216,8 @@ class ContactsFragment : Fragment() {
     private fun filterResults(input : String) {
         val filterList : ArrayList<Contact> = ArrayList()
         currentContactList.forEach {
-            it.name?.let { name ->
-                if (name.toLowerCase().
-                        contains(input.toLowerCase())) {
+            it.name.let { name ->
+                if (name.toLowerCase().contains(input.toLowerCase())) {
                     filterList.add(it)
                 }
             }
@@ -221,6 +269,18 @@ class ContactsFragment : Fragment() {
                 }
             }
         }
+    }
+
+    /**
+     * Intent to show an applications details page in (Settings) com.android.settings
+     *
+     * @param packageName   The package name of the application
+     * @return the intent to open the application info screen.
+     */
+    private fun getAppSettingsIntent(packageName: String) : Intent {
+        val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = Uri.parse("package:$packageName")
+        return intent
     }
 
 }
