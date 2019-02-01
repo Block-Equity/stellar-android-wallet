f1c180860d904e7c955ed1cca59efb5981c04058
dabitdev <imdavidfernandez@gmail.com>
2019-02-01 17:04:45 -0500

    add url to point to ios code.

--- app/src/main/java/com/blockeq/stellarwallet/models/NativeAssetAvailability.java
+++ app/src/main/java/com/blockeq/stellarwallet/models/NativeAssetAvailability.java
@@ -8,7 +8,7 @@ public interface NativeAssetAvailability extends AssetAvailability {
      *    trustlines 2 * 0.5
      *    open offer * 0.5
      *
-     *
+     * https://github.com/Block-Equity/stellar-ios-wallet/blob/ee2414061e309e2b97c4883541ca1cd335994487/StellarHub/Objects/StellarAccount.swift#L253
      *    posted for trade X
      */
     Long getBaseAmount();
